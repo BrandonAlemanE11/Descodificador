07b03acf30f7a0e9101150e71d1e08754aeaff0e
BrandonAlemanE11 <105066717+BrandonAlemanE11@users.noreply.github.com>
2022-05-29 18:10:18 -0500

    FIN

--- Descodificador/Form1.cs
+++ Descodificador/Form1.cs
@@ -26,14 +26,21 @@ namespace Descodificador
         {
 
         }
-
+        int intentos = 0;
         private void btnEntrar_Click(object sender, EventArgs e)
         {
+           
             string usuario = "brandon@upt.com"; 
             string pass = "0987654321";
             string Cusuario = txbUsuario.Text;
             string Cpass = txbContrasenia.Text;
 
+            if (intentos == 5)
+            {
+                label2.ForeColor = Color.Red;
+                label2.Text = "Demasiados Intentos";
+                btnEntrar.Enabled = false;
+            }
 
             if ( Cusuario == usuario && Cpass == pass)
             {
@@ -43,9 +50,15 @@ namespace Descodificador
             else
             {
                 label2.Text = "El Usuario u Contraseña son incorrectos";
+                intentos = intentos + 1;
             }
             
-
+            if(intentos >= 5)
+            {
+                label2.ForeColor = Color.Red;
+                label2.Text = "Demasiados Intentos";
+                btnEntrar.Enabled = false;
+            }
         }
     }
 }
